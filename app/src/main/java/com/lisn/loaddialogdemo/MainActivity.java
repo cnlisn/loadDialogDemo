58706b26663235c92ad1aa586d1d45aa1accb171
cnlisn <cnlishan@163.com>
2019-12-04 10:45:41 +0800

    添加多手指在触摸测试

--- app/src/main/java/com/lisn/loaddialogdemo/MainActivity.java
+++ app/src/main/java/com/lisn/loaddialogdemo/MainActivity.java
@@ -7,11 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
 
 import com.lisn.loaddialog.CustomDialog;
 import com.lisn.loaddialog.LoadDialogHelper;
